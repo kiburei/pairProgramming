5c1bf576b2eb77351e042d3b6a7ed5befcfefece
kiburei <akiburei@gmail.com>
2017-03-08 15:59:35 +0300

    loop through dail pairs

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
   $("#register").submit(function(event){
     event.preventDefault();
-    var students = [];
+    var students = ["Andrew", "Sotty", "Mike", "Suheib", "Paul",  "Steve", "Kevin", "Tevin", "Yonko", "Keneth", "Minner", "Ken", "Vivian", "Isiah", "Gor"];
     var uname = $("#inputUsername").val();
     var pword = $("#inputPassword").val();
 
-    if (student.firstName.includes(uname) && pword === student.password) {
+    if (students.includes(uname) && pword === "p") {
       $(".success").text("Log in successfull");
       window.location.replace("home.html");
+      var pairs = pairwise(students);
+
+      console.log(days(pairs));
+
+
 
     } else {
       $(".success").text("Username or Passord does not match");
@@ -17,21 +22,45 @@ $(document).ready(function(){
 
 
   });
-    var match = pair(student.firstName);
-    $("#pair").text(match);
+
 });
 
-var student = {
-    firstName: ["Andrew", "Sotty", "Mike", "Suheib", "Paul",  "Steve", "Kevin", "Tevin", "Yonko", "Keneth", "Minner", "Ken", "Vivian", "Isiah", "Gor"],
-    lastName: "Moringa",
-    password: "password",
-    cohort: "mftp2",
-    track: ["HTML","JavaScript","CSS"]
-  };
 
-var pair = function(std) {
-  var pairs = [];
-  pairs.push(_.sample(std, 2));
-  return pairs[0];
-
+// get all possible combinations of pairs from call list
+function pairwise(list) {
+  if (list.length < 2) { return []; }
+  var first = list[0],
+      rest  = list.slice(1),
+      pairs = rest.map(function (x) { return [first, x]; });
+  return pairs.concat(pairwise(rest));
 }
+
+// make arrays for all days
+function days(pairs) {
+  var students = ["Andrew", "Sotty", "Mike", "Suheib", "Paul",  "Steve", "Kevin", "Tevin", "Yonko", "Keneth", "Minner", "Ken", "Vivian", "Isiah", "Gor"];
+  debugger;
+    var monday = [];
+    // pick names once and return with first pair
+    monday.push(pairs[0]);
+    for (var i = 0; i < students.length; i++) {
+       if (monday[i].includes(students[i] && students[i + 2])) {
+         console.log(monday[i]);
+          // Find next student in List who is not in monday
+          var next = students[i + 2];
+          monday.push[p[i + 2]];
+
+       }
+
+    }
+  }
+
+// Check if Name already paird
+
+
+
+
+
+//
+// for (var i = 0; i < p.length; i++) {
+//   console.log(p[i]);
+// }

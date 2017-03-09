2226ea0952882262a3164dff695a58728dec8cb8
kiburei <akiburei@gmail.com>
2017-03-09 16:43:26 +0300

    pairProgramming -v 1.0

--- js/pairwise.js
+++ js/pairwise.js
@@ -3,27 +3,43 @@ $(document).ready(function(){
     event.preventDefault();
     var student = $("#pair").val();
 
+    var myPairs = ["Sotty", "Mike", "Suheib", "Paul",  "Steve", "Kevin", "Tevin"];
+
     var partner = pair(student);
+    if (myPairs.includes(partner) || partner === student) {
+      // alert("You out of luck");
+      partner = pair(student);
+    }
+    myPairs.push(partner);
+    console.log(myPairs);
     $("#match").text(student + " pairs with " + partner);
 
   });
 });
 
-
+// Business Logic
 var pair = function(student){
-  var prep = ["AK47 Majid", "Andrew", "Sotty", "Mike", "Suheib", "Paul",  "Steve", "Kevin", "Tevin", "Yonko", "Keneth", "Minner", "Ken", "Vivian", "Isiah", "Gor"];
+  var prep = ["AK47 Majid", "Andrew", "Sotty", "Mike", "Suheib", "Paul",  "Steve", "Kevin", "Tevin", "Yonko", "Keneth", "Minneh", "Ken", "Vivian", "Isiah", "Gor"];
+  debugger;
+  var myPairs = ["Sotty", "Mike", "Suheib", "Paul",  "Steve", "Kevin", "Tevin"];
+  var pairsInJson;
+  // Pick random class mate
    var partner = _.sample(prep);
-   if (student !== partner ) {
-     var index = prep.indexOf(partner);
-     //  remove student from list
-        if (index !== -1) {
-            prep.splice(index, 1);
-            console.log(prep);
-        }
-
-        return partner;
-
+  //  Check if class mate is paired with before or if classmate is yourself
+   if (myPairs.includes(partner) || partner === student) {
+    //  alert("finding other match");
+     var partner = _.sample(prep);
+       var index = prep.indexOf(partner);
+       //  add student to paired list
+          if (index !== -1) {
+              myPairs.push(partner);
+              pairsInJson = Object.assign({}, myPairs);
+              // alert(pairsInJson);
+              // console.log(myPairs);
+          }
 
    }
+   return partner;
+
 
 }
--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,6 @@ $(document).ready(function(){
     if (students.includes(uname) && pword === "p") {
       $(".success").text("Log in successfull");
       window.location.replace("home.html");
-      var pairs = pairwise(students);
-      var day = days(pairs);
-
-      $("#pair").text(students[0] + " and " + students[1]);
-
-
 
     } else {
       $(".success").text("Username or Passord does not match");
@@ -25,35 +19,3 @@ $(document).ready(function(){
   });
 
 });
-
-
-// get all possible combinations of pairs from call list
-function pairwise(list) {
-  if (list.length < 2) { return []; }
-  var first = list[0],
-      rest  = list.slice(1),
-      pairs = rest.map(function (x) { return [first, x]; });
-  return pairs.concat(pairwise(rest));
-}
-
-
-// make arrays for all days
-function days(pairs) {
-  var students = ["Andrew", "Sotty", "Mike", "Suheib", "Paul",  "Steve", "Kevin", "Tevin", "Yonko", "Keneth", "Minner", "Ken", "Vivian", "Isiah", "Gor"];
-  debugger;
-    var current_day = [];
-     for (var i = 0; i < students.length; i++) {
-       console.log(current_day.push(pairs[i]));
-     }
-
-  }
-
-
-
-
-
-
-//
-// for (var i = 0; i < p.length; i++) {
-//   console.log(p[i]);
-// }
